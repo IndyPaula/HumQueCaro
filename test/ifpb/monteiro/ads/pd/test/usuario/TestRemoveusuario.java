dace69e120aeaeb36af873987b3c5ed9b3294f85
Luiz Antonio <luiz.antonioPS95@gmail.com>
2014-08-30 14:18:32 -0300

    Testes do método para a remoção do usuário.

--- test/ifpb/monteiro/ads/pd/test/usuario/TestRemoveusuario.java
+++ test/ifpb/monteiro/ads/pd/test/usuario/TestRemoveusuario.java
@@ -1,5 +1,6 @@
 package ifpb.monteiro.ads.pd.test.usuario;
 
+import ifpb.monteiro.ads.pd.exceptions.HumQueCaroException;
 import ifpb.monteiro.ads.pd.fachada.Fachada;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -24,6 +25,11 @@ public class TestRemoveusuario {
     @Before
     public void setUp() {
         fachada = new Fachada();
+        try {
+            fachada.removeUsuario("luiz.antonioPS95@gmail.com", "senha");
+        } catch (HumQueCaroException e) {
+            fail("Acusou erro no removeUsuário: " + e.getMessage());
+        }
     }
     
     @After
@@ -31,5 +37,81 @@ public class TestRemoveusuario {
     }
 
     @Test
-    public void testRemoverUsuario() {}
+    public void testRemoverUsuario() {
+    
+        //Valores nulos ou vazios
+        try {
+            fachada.removeUsuario(null, null);
+            fail("Não era para remover Usuário - email nulo");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("", null);
+            fail("Não era para remover Usuário - email vazio");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luizAntonio@hotmail.com", null);
+            fail("Não era para remover Usuário - senha nula");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luizAntonio@hotmail.com", "");
+            fail("Não era para remover Usuário - senha vazia");
+        } catch (HumQueCaroException e) {
+        }
+        
+        //Removendo com email inválido
+        try {
+            fachada.removeUsuario("luizAntonio@gmail.com", "minhaSenha");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luiz-antonio95@live.com", "senha");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luiz.antonioPS95@gmail.com", "senha");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+        
+        //Removendo com senha inválida
+        try {
+            fachada.removeUsuario("luizAntonio@hotmail.com", "minhaSenha1");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luizAntonio@hotmail.com", "minha Senha");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luizAntonio@hotmail.com", "minhasenha");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luizAntonio@hotmail.com", " minhaSenha");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+        
+        try {
+            fachada.removeUsuario("luizAntonio@hotmail.com", "senha");
+            fail("Não era para remover Usuário - email inválido");
+        } catch (HumQueCaroException e) {
+        }
+    }
 }
