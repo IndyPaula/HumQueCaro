0353ed75dabd5f5fe51289e48dc38a56690a892e
Deivid Azevedo <deividazevedo1@gmail.com>
2014-09-01 03:06:09 -0300

    Apenas retirei linas comentadas aqui

--- src/java/ifpb/monteiro/ads/pd/main/Main.java
+++ src/java/ifpb/monteiro/ads/pd/main/Main.java
@@ -6,20 +6,13 @@ import ifpb.monteiro.ads.pd.fachadaIF.FachadaIF;
 
 public class Main {
 
-	public static void main(String[] args) {
-		FachadaIF fachada = new Fachada();
-		try {
-			fachada.alteraProduto("123123", "fabricante", "fabricante");
-		} catch (HumQueCaroException e) {
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
+        FachadaIF fachada = new Fachada();
+        try {
+            fachada.alteraProduto("123123", "fabricante", "fabricante");
+        } catch (HumQueCaroException e) {
+            e.printStackTrace();
+        }
 
-		// try {
-		// fachada.adicionaProduto("asdas", "123123", "sfgsfg");
-		// fachada.removeProduto("123123");
-		// } catch (HumQueCaroException e) {
-		// e.printStackTrace();
-		// }
-	}
-
+    }
 }
