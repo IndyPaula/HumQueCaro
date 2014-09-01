d5127f35dfba11de53f0f47bce89d759f6c4d405
Deivid Azevedo <deividazevedo1@gmail.com>
2014-09-01 03:08:13 -0300

    Adicionei construtor default (precisava dele)

--- src/java/ifpb/monteiro/ads/pd/beans/Produto.java
+++ src/java/ifpb/monteiro/ads/pd/beans/Produto.java
@@ -1,40 +1,50 @@
 package ifpb.monteiro.ads.pd.beans;
 
 public class Produto {
-	
-	private int codigoProduto;
-	private String nome;
-	private String codigo;
-	private String fabricante;
-	
-	public Produto(String nome, String codigo, String fabricante) {
-		this.nome = nome;
-		this.codigo = codigo;
-		this.fabricante = fabricante;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getFabricante() {
-		return fabricante;
-	}
-	public void setFabricante(String fabricante) {
-		this.fabricante = fabricante;
-	}
-	public String getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	public int getCodigoProduto() {
-		return codigoProduto;
-	}
-	public void setCodigoProduto(int codigoProduto) {
-		this.codigoProduto = codigoProduto;
-	}
+
+    private int codigoProduto;
+    private String nome;
+    private String codigo;
+    private String fabricante;
+
+    public Produto(String nome, String codigo, String fabricante) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.fabricante = fabricante;
+    }
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public void setCodigoProduto(int codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
 }
