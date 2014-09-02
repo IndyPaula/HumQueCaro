06c8ac9da39ed91a851a734a6fa3ad3d65a3c342
Deivid Azevedo <deividazevedo1@gmail.com>
2014-09-02 16:40:03 -0300

    Acrescentei métodos de alterar e remover produtos..

--- src/java/ifpb/monteiro/ads/pd/mb/ProdutoMB.java
+++ src/java/ifpb/monteiro/ads/pd/mb/ProdutoMB.java
@@ -9,6 +9,8 @@ import ifpb.monteiro.ads.pd.beans.Produto;
 import ifpb.monteiro.ads.pd.exceptions.HumQueCaroException;
 import ifpb.monteiro.ads.pd.fachada.FachadaBD;
 import ifpb.monteiro.ads.pd.messages.Messages;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
@@ -17,12 +19,11 @@ import javax.faces.model.ListDataModel;
 
 @SessionScoped
 @ManagedBean(name = "ProdutoMB")
-public class ProdutoMB {
+public class ProdutoMB implements java.io.Serializable {
 
     FachadaBD fachada;
     private Produto produto;
     private DataModel model;
-    private String codigo, atributo, novoValor;
     ProdutoDAO pDAO;
 
     public ProdutoMB() {
@@ -46,18 +47,6 @@ public class ProdutoMB {
         return model;
     }
 
-    public String getNovoValor() {
-        return novoValor;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getAtributo() {
-        return atributo;
-    }
-
     public DataModel getTodosProdutos() throws HumQueCaroException {
         model = new ListDataModel(pDAO.getProdutos());
         return model;
@@ -76,10 +65,22 @@ public class ProdutoMB {
 
     public String alterarProduto(ActionEvent actionEvent) {
         try {
-            fachada.alteraProduto(codigo, atributo, novoValor);
+            fachada.alteraProduto(produto.getCodigo(), produto.getNome(), produto.getFabricante());
             Messages.mensInfo("Produto alterado com sucesso!");
         } catch (HumQueCaroException ex) {
             Messages.mensInfo("Produto não alterado! Tente Novamente!" + ex.getMessage());
+        }
+        return "home.xhtml";
+    }
+
+    public String removerProduto(ActionEvent actionEvent) {
+        try {
+            Produto temp = fachada.buscaProduto(produto.getCodigo());
+            if (temp != null) {
+                fachada.removeProduto(temp);
+            }
+        } catch (HumQueCaroException ex) {
+            Logger.getLogger(ProdutoMB.class.getName()).log(Level.SEVERE, null, ex);
         }
         return "home.xhtml";
     }
