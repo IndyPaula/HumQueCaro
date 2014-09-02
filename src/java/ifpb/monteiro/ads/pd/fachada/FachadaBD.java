35f7108043480c0df99194ff23b28c9f5d1293e6
Deivid Azevedo <deividazevedo1@gmail.com>
2014-09-02 17:38:19 -0300

    alteração nas verificações de alteraProduto();

--- src/java/ifpb/monteiro/ads/pd/fachada/FachadaBD.java
+++ src/java/ifpb/monteiro/ads/pd/fachada/FachadaBD.java
@@ -13,113 +13,112 @@ import ifpb.monteiro.ads.pd.validacao.Validacao;
 
 public class FachadaBD implements FachadaBancoIF {
 
-	private DAO<Cliente> cliDAO;
-	private DAO<Produto> pDAO;
-	private DAO<Usuario> pUsuario;
+    private DAO<Cliente> cliDAO;
+    private DAO<Produto> pDAO;
+    private DAO<Usuario> pUsuario;
 
-	public FachadaBD() {
-		cliDAO = new ClienteDAO();
-		pDAO = new ProdutoDAO();
-		pUsuario = new UsuarioDAO();
-	}
+    public FachadaBD() {
+        cliDAO = new ClienteDAO();
+        pDAO = new ProdutoDAO();
+        pUsuario = new UsuarioDAO();
+    }
 
-	@Override
-	public void addUsuario(Usuario usuario) throws HumQueCaroException {
-		pUsuario.adiciona(usuario);
-	}
+    @Override
+    public void addUsuario(Usuario usuario) throws HumQueCaroException {
+        pUsuario.adiciona(usuario);
+    }
 
-	@Override
-	public void removeUsuario(Usuario usuario) throws HumQueCaroException {
-		pUsuario.remove(usuario);
-	}
+    @Override
+    public void removeUsuario(Usuario usuario) throws HumQueCaroException {
+        pUsuario.remove(usuario);
+    }
 
-	@Override
-	public Usuario buscaUsuario(String email) throws HumQueCaroException {
-		return pUsuario.procura(email);
-	}
+    @Override
+    public Usuario buscaUsuario(String email) throws HumQueCaroException {
+        return pUsuario.procura(email);
+    }
 
-	@Override
-	public void alteraUsuario(Usuario usuario) throws HumQueCaroException {
-		//Muito bem.
-		pUsuario.altera(usuario);
-	}
+    @Override
+    public void alteraUsuario(Usuario usuario) throws HumQueCaroException {
+        //Muito bem.
+        pUsuario.altera(usuario);
+    }
 
-	@Override
-	public void addProduto(Produto produto) throws HumQueCaroException {
-		pDAO.adiciona(produto);
-	}
+    @Override
+    public void addProduto(Produto produto) throws HumQueCaroException {
+        pDAO.adiciona(produto);
+    }
 
-	@Override
-	public void removeProduto(Produto produto) throws HumQueCaroException {
-		pDAO.remove(produto);
-	}
+    @Override
+    public void removeProduto(Produto produto) throws HumQueCaroException {
+        pDAO.remove(produto);
+    }
 
-	@Override
-	public Produto buscaProduto(String codigo) throws HumQueCaroException {
-		return pDAO.procura(codigo);
-	}
+    @Override
+    public Produto buscaProduto(String codigo) throws HumQueCaroException {
+        return pDAO.procura(codigo);
+    }
 
-	@Override
-	public void alteraProduto(String codigo, String atributo, String novoValor)
-			throws HumQueCaroException {
-		Validacao.validaEntrada(codigo, "Campo código inválido");
-		Validacao.validaEntrada(atributo, "Campo atributo inválido");
-		Validacao.validaEntrada(novoValor, "Campo novoValor inválido");
-		Produto produto = pDAO.procura(codigo);
-		if (atributo.equals("nomeProduto")) {
-			produto.setNome(novoValor);
-		} else if (atributo.equals("fabricante")) {
-			produto.setFabricante(novoValor);
-		} else {
-			throw new HumQueCaroException("Campo atributo invalido");
-		}
-		pDAO.altera(produto);
-	}
+    @Override
+    public void alteraProduto(String codigo, String atributo, String novoValor)
+            throws HumQueCaroException {
+        Validacao.validaEntrada(codigo, "Campo código inválido");
+        Validacao.validaEntrada(atributo, "Campo atributo inválido");
+        Validacao.validaEntrada(novoValor, "Campo novoValor inválido");
+        Produto produto = pDAO.procura(codigo);
+        if (atributo.equals("Nome")) {
+            produto.setNome(novoValor);
+        } else if (atributo.equals("Fabricante")) {
+            produto.setFabricante(novoValor);
+        } else {
+            throw new HumQueCaroException("Campo atributo invalido");
+        }
+        pDAO.altera(produto);
+    }
 
-	@Override
-	public void addCliente(Cliente cliente) throws HumQueCaroException {
-		cliDAO.adiciona(cliente);
+    @Override
+    public void addCliente(Cliente cliente) throws HumQueCaroException {
+        cliDAO.adiciona(cliente);
 
-	}
+    }
 
-	@Override
-	public void removeCliente(Cliente cliente) throws HumQueCaroException {
-		cliDAO.remove(cliente);
-	}
+    @Override
+    public void removeCliente(Cliente cliente) throws HumQueCaroException {
+        cliDAO.remove(cliente);
+    }
 
-	@Override
-	public Cliente buscaCliente(String telefone) throws HumQueCaroException {
-		Cliente c = cliDAO.procura(telefone);
-		if (telefone == null || telefone.equals("")) {
-			throw new HumQueCaroException("Campo telefone inv�lido");
-		}
-		if (c != null) {
-			return c;
-		}
-		return null;
-	}
+    @Override
+    public Cliente buscaCliente(String telefone) throws HumQueCaroException {
+        Cliente c = cliDAO.procura(telefone);
+        if (telefone == null || telefone.equals("")) {
+            throw new HumQueCaroException("Campo telefone inv�lido");
+        }
+        if (c != null) {
+            return c;
+        }
+        return null;
+    }
 
-	@Override
-	public void alteraCliente(String telefone, String atributo, String novoValor)
-			throws HumQueCaroException {
-		Cliente c = cliDAO.procura(telefone);
-		if (atributo == null || atributo.equals("") || novoValor == null
-				|| novoValor.equals("")) {
-			throw new HumQueCaroException("Campo inv�lido");
-		}
-		if (!atributo.equals("nome") && !atributo.equals("telefone")) {
-			throw new HumQueCaroException("Campo atributo inv�lido");
-		}
-		if (atributo.equals("nome") && (novoValor != null || novoValor != (""))) {
-			c.setNome(novoValor);
-		}
-		if (atributo.equals("telefone")
-				&& (novoValor != null || novoValor != (""))
-				&& novoValor.length() == 11) {
-			c.setTelefone(novoValor);
-		}
-		cliDAO.altera(c);
+    @Override
+    public void alteraCliente(String telefone, String atributo, String novoValor)
+            throws HumQueCaroException {
+        Cliente c = cliDAO.procura(telefone);
+        if (atributo == null || atributo.equals("") || novoValor == null
+                || novoValor.equals("")) {
+            throw new HumQueCaroException("Campo inv�lido");
+        }
+        if (!atributo.equals("nome") && !atributo.equals("telefone")) {
+            throw new HumQueCaroException("Campo atributo inv�lido");
+        }
+        if (atributo.equals("nome") && (novoValor != null || novoValor != (""))) {
+            c.setNome(novoValor);
+        }
+        if (atributo.equals("telefone")
+                && (novoValor != null || novoValor != (""))
+                && novoValor.length() == 11) {
+            c.setTelefone(novoValor);
+        }
+        cliDAO.altera(c);
 
-	}
-
+    }
 }
