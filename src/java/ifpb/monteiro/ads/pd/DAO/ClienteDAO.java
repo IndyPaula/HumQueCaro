757ea0c0986603c3dea7f54d913edd01556fa2b8
Deivid Azevedo <deividazevedo1@gmail.com>
2014-09-04 15:13:27 -0300

    Método getAll para listar Clientes - concluído

--- src/java/ifpb/monteiro/ads/pd/DAO/ClienteDAO.java
+++ src/java/ifpb/monteiro/ads/pd/DAO/ClienteDAO.java
@@ -5,87 +5,110 @@ import java.sql.SQLException;
 
 import ifpb.monteiro.ads.pd.beans.Cliente;
 import ifpb.monteiro.ads.pd.exceptions.HumQueCaroException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteDAO extends DAO<Cliente> {
 
-	public ClienteDAO() {
-	}
+    private List<Cliente> clientes;
 
-	@Override
-	public void adiciona(Cliente cli) throws HumQueCaroException {
-		try {
-			abrirBanco();
-			getStmt().executeUpdate(
-					"INSERT INTO clientes (nome, telefone) " + "VALUES('"
-							+ cli.getNome() + "','" + cli.getTelefone() + "')");
-			fecharBanco();
-		} catch (SQLException e) {
-			throw new HumQueCaroException("Cliente n�o adicionado"
-					+ e.getMessage());
-		}
+    public ClienteDAO() {
+    }
 
-	}
+    @Override
+    public void adiciona(Cliente cli) throws HumQueCaroException {
+        try {
+            abrirBanco();
+            getStmt().executeUpdate(
+                    "INSERT INTO clientes (nome, telefone) " + "VALUES('"
+                    + cli.getNome() + "','" + cli.getTelefone() + "')");
+            fecharBanco();
+        } catch (SQLException e) {
+            throw new HumQueCaroException("Cliente n�o adicionado"
+                    + e.getMessage());
+        }
 
-	@Override
-	public void remove(Cliente cli) throws HumQueCaroException {
-		try {
-			abrirBanco();
-			getStmt().executeUpdate(
-					"DELETE FROM clientes WHERE telefone like '"
-							+ cli.getTelefone() + "'");
-			fecharBanco();
-		} catch (SQLException e) {
-			throw new HumQueCaroException("Cliente n�o removido"
-					+ e.getMessage());
-		}
-	}
+    }
 
-	@Override
-	public void altera(Cliente cli) throws HumQueCaroException {
-		try {
-			abrirBanco();
-			getStmt().executeUpdate(
-					"UPDATE clientes SET nome = '" + cli.getNome()
-							+ "', telefone = '" + cli.getTelefone()
-							+ "', codigo_cliente ='" + cli.getCodigo()
-							+ "' WHERE codigo_cliente like '" + cli.getCodigo()
-							+ "'");
-			fecharBanco();
-		} catch (SQLException e) {
-			throw new HumQueCaroException("Cliente n�o alterado"
-					+ e.getMessage());
-		}
+    @Override
+    public void remove(Cliente cli) throws HumQueCaroException {
+        try {
+            abrirBanco();
+            getStmt().executeUpdate(
+                    "DELETE FROM clientes WHERE telefone like '"
+                    + cli.getTelefone() + "'");
+            fecharBanco();
+        } catch (SQLException e) {
+            throw new HumQueCaroException("Cliente n�o removido"
+                    + e.getMessage());
+        }
+    }
 
-	}
+    @Override
+    public void altera(Cliente cli) throws HumQueCaroException {
+        try {
+            abrirBanco();
+            getStmt().executeUpdate(
+                    "UPDATE clientes SET nome = '" + cli.getNome()
+                    + "', telefone = '" + cli.getTelefone()
+                    + "', codigo_cliente ='" + cli.getCodigo()
+                    + "' WHERE codigo_cliente like '" + cli.getCodigo()
+                    + "'");
+            fecharBanco();
+        } catch (SQLException e) {
+            throw new HumQueCaroException("Cliente n�o alterado"
+                    + e.getMessage());
+        }
 
-	@Override
-	public Cliente procura(String telefone) throws HumQueCaroException {
-		try {
-			abrirBanco();
-			ResultSet rs;
-			Cliente cli = null;
-			rs = getStmt().executeQuery(
-					"SELECT * FROM clientes WHERE telefone like '" + telefone
-							+ "'");
-			while (rs.next()) {
-				String codigo = rs.getString("codigo_cliente");
-				String fone = rs.getString("telefone");
-				String nome = rs.getString("nome");
-				cli = new Cliente(nome, fone);
-				cli.setCodigo(codigo);
-			}
-			fecharBanco();
-			return cli;
-		} catch (SQLException e) {
-			throw new HumQueCaroException("Cliente n�o encontrado"
-					+ e.getMessage());
-		}
-	}
+    }
+
+    @Override
+    public Cliente procura(String telefone) throws HumQueCaroException {
+        try {
+            abrirBanco();
+            ResultSet rs;
+            Cliente cli = null;
+            rs = getStmt().executeQuery(
+                    "SELECT * FROM clientes WHERE telefone like '" + telefone
+                    + "'");
+            while (rs.next()) {
+                String codigo = rs.getString("codigo_cliente");
+                String fone = rs.getString("telefone");
+                String nome = rs.getString("nome");
+                cli = new Cliente(nome, fone);
+                cli.setCodigo(codigo);
+            }
+            fecharBanco();
+            return cli;
+        } catch (SQLException e) {
+            throw new HumQueCaroException("Cliente n�o encontrado"
+                    + e.getMessage());
+        }
+    }
 
     @Override
     public List<Cliente> getAll() throws HumQueCaroException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        clientes = new ArrayList<>();
+        try {
+            abrirBanco();
+            ResultSet rs;
+            rs = getStmt().executeQuery("SELECT * FROM clientes");
+            Cliente cliente;
+            while (rs.next()) {
+                String nome = rs.getString("nome");
+                String telefone = rs.getString("telefone");
+                String codigo = rs.getString("codigo_cliente");
+                cliente = new Cliente(nome, telefone);
+                cliente.setCodigo(codigo);
+                if (!clientes.contains(cliente)) {
+                    clientes.add(cliente);
+                }
+            }
+            rs.close();
+            fecharBanco();
+            return clientes;
+        } catch (SQLException ex) {
+        }
+        return null;
     }
-
 }
