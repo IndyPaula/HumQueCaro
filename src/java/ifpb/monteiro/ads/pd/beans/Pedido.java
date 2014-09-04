0e1b0a1763aa016485b2f6cac2423872877604ba
Deivid Azevedo <deividazevedo1@gmail.com>
2014-09-04 20:00:00 -0300

    Adicionados os atributos do Bean Pedido

--- src/java/ifpb/monteiro/ads/pd/beans/Pedido.java
+++ src/java/ifpb/monteiro/ads/pd/beans/Pedido.java
@@ -3,5 +3,50 @@ package ifpb.monteiro.ads.pd.beans;
 /**
  */
 public class Pedido {
-    
+
+    private String codigo;
+    private String telefoneCliente;
+    private String codigoProduto;
+    private String situacao;
+
+    public Pedido() {
+    }
+
+    public Pedido(String telefoneCliente, String codigoProduto, String situacao) {
+        this.telefoneCliente = telefoneCliente;
+        this.codigoProduto = codigoProduto;
+        this.situacao = situacao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public void setCodigoProduto(String codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    public String getTelefoneCliente() {
+        return telefoneCliente;
+    }
+
+    public void setTelefoneCliente(String telefoneCliente) {
+        this.telefoneCliente = telefoneCliente;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
 }
