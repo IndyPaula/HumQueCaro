857e9993ddcc3c7b36348d5221d202b09272a1a4
Widancassio <widancassio.ads@gmail.com>
2014-09-06 18:43:05 -0300

    adicionei um bean de login

---
+++ src/java/ifpb/monteiro/ads/pd/beans/Login.java
@@ -0,0 +1,26 @@
+package ifpb.monteiro.ads.pd.beans;
+
+public class Login {
+
+    private String email;
+    private String senha;
+
+    public Login() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
