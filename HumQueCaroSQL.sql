67c154aae0dbf99fa6b8d84e87335cfe1d341c53
Luiz Antonio <luiz.antonioPS95@gmail.com>
2014-09-06 23:58:12 -0300

    Verificação de testes com Git no NetBeans

--- HumQueCaroSQL.sql
+++ HumQueCaroSQL.sql
@@ -19,12 +19,12 @@ create table usuarios(
 -- CRIA TABELA PRODUTO
 create table produtos(
 	codigo int not null auto_increment,
-	codigo_barras varchar(60) not null,
+	codigo_produto varchar(60) not null,
 	nome varchar(80) not null,
 	fabricante varchar(80) not null,
 	valor varchar(10) not null,
 	PRIMARY KEY (codigo),
-	UNIQUE (codigo_barras)
+	UNIQUE (codigo_produto)
 );
 
 
@@ -53,6 +53,6 @@ create table produtos_de_pedido(
 	codigo_produto varchar(60) not null,
 
 	FOREIGN KEY (codigo_pedido) references pedidos(codigo),
-	FOREIGN KEY (codigo_produto) references produtos(codigo_barras)
+	FOREIGN KEY (codigo_produto) references produtos(codigo_produto)
 )
 
