a5096c0de76e4fdd96ec8a5a15c8aec29060dca6
Widancassio <widancassio.ads@gmail.com>
2014-09-09 00:26:36 -0300

    Novo método adicionado

--- src/java/ifpb/monteiro/ads/pd/gerenciadores/GerenciadorLoginIF.java
+++ src/java/ifpb/monteiro/ads/pd/gerenciadores/GerenciadorLoginIF.java
@@ -1,8 +1,11 @@
 package ifpb.monteiro.ads.pd.gerenciadores;
 
+import ifpb.monteiro.ads.pd.beans.Usuario;
 import ifpb.monteiro.ads.pd.exceptions.HumQueCaroException;
 
 public interface GerenciadorLoginIF {
 
     public void fazerLogin(String email, String senha) throws HumQueCaroException;
+    
+    public void logado() throws HumQueCaroException;
 }
