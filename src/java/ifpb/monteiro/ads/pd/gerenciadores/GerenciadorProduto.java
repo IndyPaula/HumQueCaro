7aac1770f845fafc5df58619395dadc3d88045ff
Widancassio <widancassio.ads@gmail.com>
2014-09-09 00:25:48 -0300

    Verificação de logado() adicionada a todos os métodos das funcionalidades que o usuário deve ta logado para utilizalos

--- src/java/ifpb/monteiro/ads/pd/gerenciadores/GerenciadorProduto.java
+++ src/java/ifpb/monteiro/ads/pd/gerenciadores/GerenciadorProduto.java
@@ -1,6 +1,8 @@
 package ifpb.monteiro.ads.pd.gerenciadores;
 
+import ifpb.monteiro.ads.pd.beans.Login;
 import ifpb.monteiro.ads.pd.beans.Produto;
+import ifpb.monteiro.ads.pd.beans.Usuario;
 import ifpb.monteiro.ads.pd.enumerations.Dados;
 import ifpb.monteiro.ads.pd.exceptions.HumQueCaroException;
 import ifpb.monteiro.ads.pd.fachada.FachadaBD;
@@ -11,32 +13,36 @@ import java.util.List;
 public class GerenciadorProduto implements GerenciadorProdutoIF {
 
     private FachadaBancoIF pDAO;
+    private GerenciadorLoginIF gLogin;
 
     public GerenciadorProduto() {
         pDAO = new FachadaBD();
+        gLogin = GerenciadorLogin.getInstance();
     }
 
     @Override
     public void adicionaProduto(String nomeProduto, String codigo,
             String fabricante, String valor) throws HumQueCaroException {
+        gLogin.logado();
         Validacao.validaEntrada(nomeProduto, "Campo nome do produto invalido");
         Validacao.validaEntrada(codigo, "Campo codigo invalido");
         Validacao.validaEntrada(fabricante, "Campo fabricante invalido");
         Validacao.validaEntrada(valor, "Campo valor invalido");
-        try {
-            if (pDAO.buscaProduto(codigo) == null) {
-                pDAO.addProduto(new Produto(nomeProduto, codigo, fabricante, valor));
-            } else {
-                throw new HumQueCaroException("Produto já cadastrado");
+            try {
+                if (pDAO.buscaProduto(codigo) == null) {
+                    pDAO.addProduto(new Produto(nomeProduto, codigo, fabricante, valor));
+                } else {
+                    throw new HumQueCaroException("Produto já cadastrado");
+                }
+            } catch (HumQueCaroException e) {
+                throw new HumQueCaroException(e.getMessage());
             }
-        } catch (HumQueCaroException e) {
-            throw new HumQueCaroException(e.getMessage());
         }
-    }
 
     @Override
     public void alteraProduto(String codigo, String atributo, String novoValor)
             throws HumQueCaroException {
+        gLogin.logado();
         Validacao.validaEntrada(codigo, "Campo código inválido");
         Validacao.validaEntrada(atributo, "Campo atributo inválido");
         Validacao.validaEntrada(novoValor, "Campo novoValor inválido");
@@ -55,6 +61,7 @@ public class GerenciadorProduto implements GerenciadorProdutoIF {
 
     @Override
     public void removeProduto(String codigo) throws HumQueCaroException {
+        gLogin.logado();
         buscaProduto(codigo);
         pDAO.removeProduto(codigo);
     }
