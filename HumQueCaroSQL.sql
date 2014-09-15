d8aa54e7e0b9525a752e3ac2a71ec7be71762078
Luiz Antonio <luiz.antonioPS95@gmail.com>
2014-09-15 09:48:13 -0300

    Alterações necessários no SQL com adições de constraints.
    Bom, adicionei algumas constraint nas tabelas de pedido e de produtos_de_pedido. Assim:
      - Quando um pedido for excluído, suas pendências na tabela produtos_de_pedido também serão exclusos.
      - Quando um cliente ou um produto for excluso, será setado null nos campos onde eles são referenciados nas tabelas de Pedido e Produtos_de_pedido, respectivamente.

--- HumQueCaroSQL.sql
+++ HumQueCaroSQL.sql
@@ -11,7 +11,7 @@ create table usuarios(
 	email varchar(60) not null,
 	nome varchar(80) not null,
 	senha varchar(30) not null,
-	PRIMARY KEY (codigo),
+	constraint codigoUs_pk PRIMARY KEY (codigo),
 	UNIQUE (email)
 );
 
@@ -23,7 +23,7 @@ create table produtos(
 	nome varchar(80) not null,
 	fabricante varchar(80) not null,
 	valor varchar(10) not null,
-	PRIMARY KEY (codigo),
+	constraint codigoPro_pk PRIMARY KEY (codigo),
 	UNIQUE (codigo_barras)
 );
 
@@ -33,27 +33,26 @@ create table clientes(
 	codigo int not null auto_increment,
 	telefone char(11) not null,
 	nome varchar(80) not null,
-	PRIMARY KEY (codigo),
+	constraint codigoCli_pk PRIMARY KEY (codigo),
 	UNIQUE (telefone)
 );
 
 -- CRIA TABELA PEDIDO
 create table pedidos(
 	codigo int not null auto_increment,
-	telefone_cliente char(11) not null,
+	telefone_cliente char(11),
 	situacao varchar(10) not null,
 	valor float not null,
 	
-	PRIMARY KEY(codigo),
-	FOREIGN KEY (telefone_cliente) references clientes(telefone)
+	constraint codigoPed_pk PRIMARY KEY(codigo),
+	constraint set_null_telefone FOREIGN KEY (telefone_cliente) references clientes(telefone) ON DELETE SET null
 );
 
 -- CRIA TABELA PRODUTOS_DE_PEDIDO
 create table produtos_de_pedido(
 	codigo_pedido int not null,
-	codigo_produto varchar(60) not null,
+	codigo_produto varchar(60),
 
-	FOREIGN KEY (codigo_pedido) references pedidos(codigo),
-	FOREIGN KEY (codigo_produto) references produtos(codigo_barras)
+	constraint cascade_codigoPed FOREIGN KEY (codigo_pedido) references pedidos(codigo) ON DELETE cascade,
+	constraint set_null_codigo FOREIGN KEY (codigo_produto) references produtos(codigo_barras) ON DELETE SET null
 )
-
